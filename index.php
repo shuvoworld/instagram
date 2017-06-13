ac19815a60807dbcd80bac4bfa3807c84ecf4df5
Enamul Haque <shuvoworld@gmail.com>
2017-06-14 02:09:24 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+$client_id = 'Your client id';
+$client_secret = 'Your client secret';
+$redirect_uri = 'http://localhost/photoapp';
+$scope = 'basic+likes+comments+relationships';
+
+$url = "https://api.instagram.com/oauth/authorize?client_id=$client_id&redirect_uri=$redirect_uri&scope=$scope&response_type=code";
+
+if(!isset($_GET['code']))
+{
+    echo '<a href="'.$url.'">Login With Instagram</a>';
+}
+else
+{
+    $code = $_GET['code'];
+
+$apiData = array(
+  'client_id'       => $client_id,
+  'client_secret'   => $client_secret,
+  'grant_type'      => 'authorization_code',
+  'redirect_uri'    => $redirect_uri,
+  'code'            => $code
+);
+
+$apiHost = 'https://api.instagram.com/oauth/access_token';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $apiHost);
+curl_setopt($ch, CURLOPT_POST, count($apiData));
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($apiData));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);  
+curl_setopt($ch,CURLOPT_SSL_VERIFYHOST, false);
+$jsonData = curl_exec($ch);
+curl_close($ch);
+echo "<pre>";
+print_r($jsonData);
+echo '<pre>';
+exit;
+}
+?>
